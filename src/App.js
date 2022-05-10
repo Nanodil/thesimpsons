7f21f455bb7d3ac0cc88e2a3365d8917674471c4
nano <marianodilella86@gmail.com>
2022-05-09 22:12:23 -0300

    agrego el request de las api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import logo from './logo.png';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from "./logo.png";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Frase from "./components/Frase";
+import { useState, useEffect } from "react";
 
 function App() {
- return (
-   <section>
-     <img src={logo} alt="logo de los simpsons" />
-   </section>
- );
+  const [personaje, setPersonaje] = useState({});
+
+  useEffect(() => {
+    consultarAPI();
+  },[]);
+
+  const consultarAPI = async() => {
+    const respuesta = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes");
+    const dato = await respuesta.json();
+    console.log(respuesta);
+    console.log(dato[0]);
+    //guardar en el state
+    setPersonaje(dato[0]);
+  };
+
+  return (
+    <section className="container my-5 d-flex flex-column align-items-center">
+      <img src={logo} alt="logo de los simpsons" width={300} />
+      <Button variant="warning" className="my-5 w-25">
+        Obtener frase
+      </Button>
+      <Frase></Frase>
+    </section>
+  );
 }
 
 export default App;
