ac094a1b180f2a5a488caf60a7db463827d4864c
nano <marianodilella86@gmail.com>
2022-05-10 18:15:37 -0300

    trabajo listo

--- src/App.js
+++ src/App.js
@@ -3,31 +3,39 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import Frase from "./components/Frase";
+import Spinner from "./components/Spinner";
 import { useState, useEffect } from "react";
 
 function App() {
   const [personaje, setPersonaje] = useState({});
-
+const [carga, setCarga] =useState(false);
   useEffect(() => {
     consultarAPI();
   },[]);
 
   const consultarAPI = async() => {
+    setCarga(false);
     const respuesta = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes");
     const dato = await respuesta.json();
     console.log(respuesta);
     console.log(dato[0]);
-    //guardar en el state
-    setPersonaje(dato[0]);
+    setTimeout(()=> {
+      //guardar en el state
+      setPersonaje(dato[0]);
+      //mostrar el componente frase
+      setCarga(true);
+
+    }, 1500);
   };
+//operador ternario
+// (condicion logica)?(codigo a ejecutar cuando es verdadera):(codigo cuando la condicion es falsa)
+  const mostrarComponente = (carga === true)?(<Frase personaje={personaje}></Frase>):(<Spinner/>)
 
   return (
     <section className="container my-5 d-flex flex-column align-items-center">
       <img src={logo} alt="logo de los simpsons" width={300} />
-      <Button variant="warning" className="my-5 w-25">
-        Obtener frase
-      </Button>
-      <Frase></Frase>
+      <Button variant="warning" className="my-5 w-25" onClick={()=> consultarAPI()}>Obtener frase</Button>
+      {mostrarComponente}
     </section>
   );
 }
